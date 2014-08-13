d97cfcb893c2c8e63495c352234fb91830afc3be
oyiadin <oyiadin@gmail.com>
2014-08-13 21:41:31 +0800

    喜闻乐见的破队形

--- robot.py
+++ robot.py
@@ -37,6 +37,7 @@ class Client(WebQQClient):
     def __init__(self, *args, **kwargs):
         self.keywords = True
         self.last_msg = ''
+        self.enable_break = True
         super(Client, self).__init__(*args, **kwargs)
 
     def handle_verify_code(self, path, r, uin):
@@ -53,24 +54,26 @@ class Client(WebQQClient):
             self.hub.accept_verify(from_uin, account, account)
 
     @group_message_handler
-    def handle_group_message(self, member_nick, content, group_code,
+    def handle_group_message(self, member_nick, i, group_code,
                              send_uin, source):
-        content = content.strip().lower()
-        if content == self.last_msg:
-            content = '破'
+        i = i.strip().lower()
+        last_msg = self.last_msg
+        self.last_msg = i
 
-        elif content == 'help' or content == '帮助':
+        if i == 'help' or i == '帮助':
             content = '''
 逗比机器人使用指南：
     help: 发送本指南
     help calc:  计算器使用指南
     ping: 看看机器人是否还在工作
-    start: 开启关键字监测
-    pause: 暂停关键字监测
+    start kw: 开启关键字监测
+    pause kw: 暂停关键字监测
+    start break: 开启自动破队形
+    pause break: 暂停自动破队形
     about: 关于这个项目'''
 
-        elif content == 'help calc' or content == 'calc' \
-            or content == '计算器' or content == '计算器帮助':
+        elif i == 'help calc' or i == 'calc' \
+            or i == '计算器' or i == '计算器帮助':
             content = '''
 召唤格式:式子=
 加:+ 减:-
@@ -89,7 +92,7 @@ sinh() cosh() tanh()
 圆周率:pi
 自然底数:e'''
 
-        elif content == 'ping':
+        elif i == 'ping':
             content = [
                 '小的在！',
                 '啊哈？',
@@ -102,10 +105,10 @@ sinh() cosh() tanh()
                 '想搞基？',
                 '来搞基么',
                 '喵喵喵',
-                'Thirq 在此'
+                'Thirq 在此',
                 '我感觉我萌萌哒']
 
-        elif content == 'about' or content == '关于':
+        elif i == 'about' or i == '关于':
             content = '''
 暂定名称：逗比机器人
 作者：@oyiadin
@@ -114,69 +117,78 @@ sinh() cosh() tanh()
 
 正在开发的下一代名称为 Thirq'''
 
-        elif content == 'pause' or content == '暂停':
+        elif i == 'pause kw':
             self.keywords = False
             content = '关键词监测已关闭'
 
-        elif content == 'start' or content == '开始':
+        elif i == 'pause break':
+            self.enable_break = False
+            content = '自动破队形已关闭'
+
+        elif i == 'start kw':
             self.keywords = True
             content = '关键词监测已开启'
+
+        elif i == 'start break':
+            self.enable_break = True
+            content = '自动破队形已开启'
+
+        elif i.endswith('='):
+            exp = re.sub(r'(sin|cos|tan)', r'_\1', i)
+            try:
+                content = i + str(eval(i[:-1], _math, {}))
+            except Exception, msg:
+                content = '发生错误:' + str(msg)
+            except:
+                return
+                # 我忘记 Exception 是不是所有异常的祖先了……所以为了保险
+
+        elif i == last_msg:
+            content = '破'
 
         elif not self.keywords:
              return
 
         else:
-            if content.endswith('='):
-                content = re.sub(r'(sin|cos|tan)', r'_\1', content)
-                try:
-                    content = content + str(eval(content[:-1], _math, {}))
-                except Exception, msg:
-                    content = '发生错误:' + str(msg)
-                except:
-                    return
-                    # 我忘记 Exception 是不是所有异常的祖先了……所以为了保险
-
-            elif '机器人' in content:
+            if '机器人' in i:
                 content = [
                     '你才是机器人，你全家都是机器人',
                     '呵呵呵机器人怎么了？机器人怎么了你说呀',
                     '机器人万岁。',
                     '人类是渣渣。',
                     'I wanna fuck u :)']
-            elif '群主' in content:
+            elif '群主' in i:
                 content = [
                     '群主啊，这东西很好吃',
                     '群主是我',
                     '群主是 S[哗————]B']
-            elif '勾搭' in content:
+            elif '勾搭' in i:
                 content = ['搭车球勾搭', '我的主人也想求勾搭呢']
-            elif '给我' in content:
+            elif '给我' in i:
                 content = ['好的给你']
-            elif '刷屏' in content:
+            elif '刷屏' in i:
                 content = [
                     '谁刷屏？',
                     '刷屏不好玩啊，那样会让我被腾讯欺负的（泪眼汪汪）',
                     '刷屏去shi']
-            elif '好激动' in content:
+            elif '好激动' in i:
                 content = [
                     '昨晚干啥了那么激动？',
                     '我也好激动',
                     '刚把《在物质加速到亚光速的过程中其温度的变化》写完了，我也好激动0w0']
-            elif '搞基' in content:
+            elif '搞基' in i:
                 content = ['和我搞怎么样', '搞基这事可不能说得太细', '啊，考虑一下我？']
-            elif '你好' in content:
+            elif '你好' in i:
                 content = ['我很好', '我不好', '我没妹子怎么好得起来']
-            elif '爆照' in content:
+            elif '爆照' in i:
                 content = ['快爆', '爆吧我正在准备储存呢']
-            elif '主人' in content:
+            elif '主人' in i:
                 content = ['我主人超帅']
             else:
                 return
 
         if isinstance(content, list):
             content = random.choice(content)
-
-        self.last_msg = content
 
         self.hub.send_group_msg(group_code, u"@{0}: {1}".format(member_nick, content))
 
