a7d8fdb1761512fc64295fe2a9c5fa6a5731d812
oyiadin <oyiadin@gmail.com>
2014-08-13 20:40:25 +0800

    小改动

--- robot.py
+++ robot.py
@@ -9,7 +9,6 @@ from __future__ import division
 
 import logging
 import math
-math = vars(math)
 
 from twqq.client import WebQQClient
 from twqq.requests import system_message_handler, group_message_handler
@@ -26,10 +25,19 @@ sys.setdefaultencoding('UTF-8')
 
 
 logger = logging.getLogger("client")
+from copy import copy
+
+math = vars(math)
+del math['__builtins__']
+for func in ['sin', 'cos', 'tan']:
+    math['_{0}'.format(func)] = math[func]
+    math[func] = lambda x: math['_{0}'.format(func)](math['radians'](x))
+
 
 class Client(WebQQClient):
     def __init__(self, *args, **kwargs):
         self.keywords = True
+        self.last_msg = ''
         super(Client, self).__init__(*args, **kwargs)
 
     def handle_verify_code(self, path, r, uin):
@@ -49,15 +57,18 @@ class Client(WebQQClient):
     def handle_group_message(self, member_nick, content, group_code,
                              send_uin, source):
         content = content.strip().lower()
-        if content == 'help' or content == '帮助':
+        if content == self.last_msg:
+            content = '破'
+
+        elif content == 'help' or content == '帮助':
             content = '''
 逗比机器人使用指南：
     help: 发送本指南
     help calc:  计算器使用指南
     ping: 看看机器人是否还在工作
-    start - 重新开启关键字监测
+    start: 开启关键字监测
     pause: 暂停关键字监测
-    about - 关于这个项目'''
+    about: 关于这个项目'''
 
         elif content == 'help calc' or content == 'calc' \
             or content == '计算器' or content == '计算器帮助':
@@ -69,12 +80,10 @@ class Client(WebQQClient):
 abs(x) 绝对数
 log(x,底数) 对数
 sqrt(x) 方根
-sin(r) 正弦*
-cos(r) 余弦*
-tan(r) 正切*
-*  三角函数括号内的数值为弧度，
-   要使用之前需要使用 radians(角度值) 进行转换。
-   比如 sin(45°) 需写 sin(radians(45))=
+sin(r) 正弦
+cos(r) 余弦
+tan(r) 正切
+*  以上三个函数括号内的数值为角度值
 以下不解释，需要用的自会看懂：
 asin() acos() atan()
 sinh() cosh() tanh()
@@ -93,13 +102,16 @@ sinh() cosh() tanh()
                 '我还在线着呢',
                 '想搞基？',
                 '来搞基么',
-                '喵喵喵']
+                '喵喵喵',
+                'Thirq 在此'
+                '我感觉我萌萌哒']
 
         elif content == 'about' or content == '关于':
             content = '''
 暂定名称：逗比机器人
-作者：@oyiadin(陈晓源)
+作者：@oyiadin
 源码：https://github.com/oyiadin/doubiRobot
+作者博客：http://oyiadin.com
 
 正在开发的下一代名称为 Thirq'''
 
@@ -117,10 +129,12 @@ sinh() cosh() tanh()
 
             if content.endswith('='):
                 try:
-                    result = eval(content[:-1], math)
+                    content = content + str(eval(content[:-1], math, {}))
+                except Exception, msg:
+                    content = '发生错误:' + str(msg)
                 except:
                     return
-                content = content + str(result)
+                    # 我忘记 Exception 是不是所有异常的祖先了……所以为了保险
 
             elif '机器人' in content:
                 content = [
@@ -128,16 +142,14 @@ sinh() cosh() tanh()
                     '呵呵呵机器人怎么了？机器人怎么了你说呀',
                     '机器人万岁。',
                     '人类是渣渣。',
-                    'Error: I wanna fuck u :)']
+                    'I wanna fuck u :)']
             elif '群主' in content:
                 content = [
                     '群主啊，这东西很好吃',
                     '群主是我',
                     '群主是 S[哗————]B']
             elif '勾搭' in content:
-                content = ['搭车球勾搭']
-            elif '明白' in content or '懂了' in content:
-                content = ['明白了就好（语重心长状）', '你真的懂了？']
+                content = ['搭车球勾搭', '我的主人也想求勾搭呢']
             elif '给我' in content:
                 content = ['好的给你']
             elif '刷屏' in content:
@@ -154,13 +166,16 @@ sinh() cosh() tanh()
                 content = ['和我搞怎么样', '搞基这事可不能说得太细', '啊，考虑一下我？']
             elif '你好' in content:
                 content = ['我很好', '我不好', '我没妹子怎么好得起来']
+            elif '爆照' in content:
+                content = ['快爆', '爆吧我正在准备储存呢']
+            elif '主人' in content:
+                content = ['我主人超帅']
             else:
                 return
 
         if isinstance(content, list):
             content = random.choice(content)
 
-        logging.warn(content)
         self.hub.send_group_msg(group_code, u"@{0}: {1}".format(member_nick, content))
 
     @buddy_message_handler
@@ -174,8 +189,8 @@ if __name__ == "__main__":
     tornado.log.enable_pretty_logging()
     tornado.autoreload.start()
 
-    webqq = Client(int(sys.argv[1]), sys.argv[2])
     try:
+        webqq = Client(int(sys.argv[1]), sys.argv[2])
         webqq.run()
     except:
         print 'Exit now'
